35f4472845a7650ffe56a09a3f9ede336e181e66
Abhilash Ravi <Abhilash.ravee@gmail.com>
2021-06-01 01:44:28 +0530

    changes

--- CaseStudy_31stMay2021/CaseStudy_ConsoleApp/CaseStudy_ConsoleApp/Program.cs
+++ CaseStudy_31stMay2021/CaseStudy_ConsoleApp/CaseStudy_ConsoleApp/Program.cs
@@ -1,11 +1,9 @@
 ﻿using CustomLogicApp.FileReader;
 using System;
 using System.IO;
-<<<<<<< HEAD
 using System.Linq;
 using System.Text.RegularExpressions;
-=======
->>>>>>> ed99ae3362f9842e137f2b4d3103c393af7966cc
+
 
 namespace CaseStudy_ConsoleApp
 {
@@ -13,25 +11,20 @@ namespace CaseStudy_ConsoleApp
     {
         readonly string rootDir = "";
 
-<<<<<<< HEAD
+
         /**
          * This is the main method - Get the current directory here and then get the root directory from it
          * 
          */
+       
         public Program()
         {
-            //Checking path of the current directory
-=======
-        public Program()
-        {
->>>>>>> ed99ae3362f9842e137f2b4d3103c393af7966cc
             var mydirectory = Environment.CurrentDirectory;
             int indexofmyvar = mydirectory.IndexOf("bin");
             rootDir= mydirectory.Substring(0, indexofmyvar);
         }
         static void Main(string[] args)
         {
-<<<<<<< HEAD
 
             Program program = new Program();   
             //custom class to read files
@@ -51,13 +44,8 @@ namespace CaseStudy_ConsoleApp
             //if using Regular expressions
             var files = Directory.GetFiles(Environment.CurrentDirectory).Where(file => Regex.IsMatch(file, @"^.+\.(xml|xslt|xsl)$"));
             Console.WriteLine(customDataReader.GetResult(files));
-=======
-            Program program = new Program();
-            CustomDataReader customDataReader = new CustomDataReader();
-            Console.WriteLine("Welcome to Trisoft--->SDL TriSoft Case Study App!!");
-            string[] fileArray = Directory.GetFiles(Environment.CurrentDirectory, "*.xml", SearchOption.AllDirectories);
-            Console.WriteLine(customDataReader.GetResult(fileArray));
->>>>>>> ed99ae3362f9842e137f2b4d3103c393af7966cc
+
+
         }
         
     }
